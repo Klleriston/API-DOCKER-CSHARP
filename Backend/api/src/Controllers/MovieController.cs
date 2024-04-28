10b2b32bd65f52d4eebcdb498ef2328e33abba66
Klleriston <klleandrade@Outlook.com>
2024-04-28 19:42:53 -0300

    fix: updated MovieRepository

--- Backend/api/src/Controllers/MovieController.cs
+++ Backend/api/src/Controllers/MovieController.cs
@@ -9,16 +9,14 @@ namespace src.Controllers
     [Route("/")]
     public class MovieController : Controller
     {
-       private readonly IMovieRepository _movieRepository;
-       private readonly MovieNetContext _movieNetContext;
+        private readonly MovieNetContext _movieNetContext;
 
-        public MovieController(IMovieRepository movieRepository, MovieNetContext movieNetContext)
+        public MovieController(MovieNetContext movieNetContext)
         {
-            _movieRepository = movieRepository;
             _movieNetContext = movieNetContext;
         }
 
-        [HttpPost("/movie")]
+        [HttpPost("/movie/create")]
         public async Task<IActionResult> createMovie([FromBody] Movie movie)
         {
             if (movie == null)
@@ -31,34 +29,55 @@ namespace src.Controllers
                 _movieRepository.Create(movie);
                 await _movieNetContext.SaveChangesAsync();
 
-                return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, new { success = true, data = movie });
+                return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, new { success = true, data = movie });
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return StatusCode(500, "ops falha aqui :p");
             }
         }
 
         [HttpGet("/movie/{id}")]
-        public async Task<IActionResult> GetMovie(int id)
+        public IActionResult GetMovieById(int id)
         {
-            var movie =  _movieRepository.GetMovieById(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
+            var movie = _movieRepository.GetMovieById(id);
             return Ok(movie);
         }
 
         [HttpGet("/movie")]
-        public async Task<IActionResult> GetMovie()
+        public async Task<IActionResult> GetMovies()
         {
             var movies = await _movieNetContext.Movies.ToListAsync();
             return Ok(movies);
         }
 
+        [HttpPut("/movie/update/{id}")]
+        public async Task<IActionResult> UpdatedMovie([FromBody] Movie movie, int id)
+        {
+            if (movie == null) 
+            {
+                return BadRequest("Mandou errado de novo mano ?");
+            }
 
+            try
+            {
+                var updatedMovie = _movieRepository.GetMovieById(id);
 
+                updatedMovie.Title = movie.Title;
+                updatedMovie.Description = movie.Description;
+                updatedMovie.Year = movie.Year;
 
+                _movieNetContext.Update(updatedMovie);
+                await _movieNetContext.SaveChangesAsync();
+
+                return Ok("Filme atualizado !!");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Deu ruim no update xD");
+            }
+        }
+
+    
     }
 }
--- Backend/api/src/Data/MovieRepository.cs
+++ Backend/api/src/Data/MovieRepository.cs
@@ -2,18 +2,19 @@
 
 namespace src.Data
 {
-    public class MovieRepository : IMovieRepository
+    public class MovieRepository
     {
         private readonly MovieNetContext _movieNetContext;
+        
         public MovieRepository(MovieNetContext movieNetContext)
         {
             _movieNetContext = movieNetContext;
         }
-        public Movie Create(Movie movie)
+     
+        public void Create(Movie movie)
         {
            _movieNetContext.Movies.Add(movie);
             movie.Id = _movieNetContext.SaveChanges();
-            return movie;
         }
 
         public Movie GetMovieById(int id)
@@ -21,11 +22,31 @@ namespace src.Data
             var movie = _movieNetContext.Movies.FirstOrDefault(m =>  m.Id == id);
             return movie;
         }
-
         public Movie GetMovieByTitle(string title)
         {
            var movie = _movieNetContext.Movies.FirstOrDefault(m =>m.Title == title);
             return movie;
+        }
+
+        public void UpdatedMovie(Movie movie, int id)
+        {
+            var updatedMovie = _movieNetContext.Movies.FirstOrDefault(m =>  m.Id == id);
+
+            updatedMovie.Title = movie.Title;
+            updatedMovie.Description = movie.Description;
+            updatedMovie.Year = movie.Year;
+
+            _movieNetContext.Update(updatedMovie);
+            _movieNetContext.SaveChanges();
+        }
+         public void DeleteMovie(Guid id)
+        {
+            var movie = _movieNetContext.Movies.Find(id);
+            if (movie != null)
+            {
+                _movieNetContext.Remove(movie);
+                _movieNetContext.SaveChanges();
+            }
         }
     }
 }
